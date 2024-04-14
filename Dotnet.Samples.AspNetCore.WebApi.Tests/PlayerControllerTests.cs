77575166f82cec21b170bf1f39ba64c835d2f28c
Nano Taboada <87288+nanotaboada@users.noreply.github.com>
2024-04-14 18:40:42 -0300

    feature(tests): add cases for HttpGet methods

--- Dotnet.Samples.AspNetCore.WebApi.Tests/PlayerControllerTests.cs
+++ Dotnet.Samples.AspNetCore.WebApi.Tests/PlayerControllerTests.cs
@@ -15,25 +15,98 @@ namespace Dotnet.Samples.AspNetCore.WebApi.Tests;
 public class PlayerControllerTests
 {
     [Fact]
-    [Trait("Category", "HTTP GET")]
-    public async Task GivenHttpGet_WhenRequestPathHasNoId_ThenResponseStatusCodeShouldBeListOfPlayers()
+    [Trait("Category", "GetAsync")]
+    public async Task GivenGetAsync_WhenServiceRetrieveAsyncReturnsListOfPlayers_ThenResponseShouldBeEquivalentToListOfPlayers()
     {
         // Arrange
         var players = PlayerDataBuilder.SeedWithStarting11();
-        var logger = new Mock<ILogger<PlayersController>>();
+
         var service = new Mock<IPlayerService>();
-        service.Setup(service => service.RetrieveAsync()).Returns(Task.FromResult(players));
+        service.Setup(service => service.RetrieveAsync()).ReturnsAsync(players);
+        var logger = new Mock<ILogger<PlayersController>>();
 
         var controller = new PlayersController(service.Object, logger.Object);
 
         // Act
-        var response = await controller.GetAsync();
+        var result = await controller.GetAsync();
 
         // Assert
         service.Verify(service => service.RetrieveAsync(), Times.Exactly(1));
-        response.Should().BeOfType<Ok<List<Player>>>();
-        var result = response as Ok<List<Player>>;
-        result?.StatusCode.Should().Be(StatusCodes.Status200OK);
-        result?.Value.Should().BeEquivalentTo(players);
+        result.Should().BeOfType<Ok<List<Player>>>();
+        var response = result as Ok<List<Player>>;
+        response?.StatusCode.Should().Be(StatusCodes.Status200OK);
+        response?.Value.Should().BeOfType<List<Player>>();
+        response?.Value.Should().BeEquivalentTo(players);
+    }
+
+    [Fact]
+    [Trait("Category", "GetAsync")]
+    public async Task GivenGetAsync_WhenServiceRetrieveAsyncReturnsEmptyList_ThenResponseStatusCodeShouldBe404NotFound()
+    {
+        // Arrange
+        var players = new List<Player>(); // Count = 0
+
+        var service = new Mock<IPlayerService>();
+        service.Setup(service => service.RetrieveAsync()).ReturnsAsync(players);
+        var logger = new Mock<ILogger<PlayersController>>();
+
+        var controller = new PlayersController(service.Object, logger.Object);
+
+        // Act
+        var result = await controller.GetAsync();
+
+        // Assert
+        service.Verify(service => service.RetrieveAsync(), Times.Exactly(1));
+        result.Should().BeOfType<NotFound>();
+        var response = result as NotFound;
+        response?.StatusCode.Should().Be(StatusCodes.Status404NotFound);
+    }
+
+    [Fact]
+    [Trait("Category", "GetByIdAsync")]
+    public async Task GivenGetByIdAsync_WhenServiceRetrieveByIdAsyncReturnsNull_ThenResponseStatusCodeShouldBe404NotFound()
+    {
+        // Arrange
+        var player = (Player?)null;
+
+        var service = new Mock<IPlayerService>();
+        service.Setup(service => service.RetrieveByIdAsync(It.IsAny<long>())).ReturnsAsync(player);
+        var logger = new Mock<ILogger<PlayersController>>();
+
+        var controller = new PlayersController(service.Object, logger.Object);
+
+        // Act
+        var result = await controller.GetByIdAsync(It.IsAny<long>());
+
+        // Assert
+        service.Verify(service => service.RetrieveByIdAsync(It.IsAny<long>()), Times.Exactly(1));
+        result.Should().BeOfType<NotFound>();
+        var response = result as NotFound;
+        response?.StatusCode.Should().Be(StatusCodes.Status404NotFound);
+    }
+
+    [Fact]
+    [Trait("Category", "GetByIdAsync")]
+    public async Task GivenGetByIdAsync_WhenServiceRetrieveByIdAsyncReturnsPlayer_ThenResponseStatusCodeShouldBe200Ok()
+    {
+        // Arrange
+        var player = PlayerDataBuilder.SeedOneById(10);
+
+        var service = new Mock<IPlayerService>();
+        service.Setup(service => service.RetrieveByIdAsync(It.IsAny<long>())).ReturnsAsync(player);
+        var logger = new Mock<ILogger<PlayersController>>();
+
+        var controller = new PlayersController(service.Object, logger.Object);
+
+        // Act
+        var result = await controller.GetByIdAsync(It.IsAny<long>());
+
+        // Assert
+        service.Verify(service => service.RetrieveByIdAsync(It.IsAny<long>()), Times.Exactly(1));
+        result.Should().BeOfType<Ok<Player>>();
+        var response = result as Ok<Player>;
+        response?.StatusCode.Should().Be(StatusCodes.Status200OK);
+        response?.Value.Should().BeOfType<Player>();
+        response?.Value.Should().BeEquivalentTo(player);
     }
 }
